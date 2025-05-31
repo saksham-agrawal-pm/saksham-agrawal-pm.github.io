e9bee2218032b6a0ac35e0e3ffb55cfd47a00ff7
Saksham Agrawal <saksham@jivi.ai>
2025-05-31 15:59:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const profilePicture = document.getElementById('profile-picture');
+    const fullName = document.getElementById('full-name');
 
+    try {
+        const response = await fetch('https://api.github.com/users/saksham-agrawal-pm');
+        const data = await response.json();
+
+        profilePicture.src = data.avatar_url;
+        fullName.textContent = data.name;
+    } catch (error) {
+        console.error('Error fetching GitHub profile:', error);
+    }
+});
